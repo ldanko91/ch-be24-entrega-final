b1a515a0e92924a4c466bdd15f0448ce1bf09cc4
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2024-05-27 21:53:29 -0300

    entrega-final-v0.9-mongoConnection-hardcoded

--- src/utils/mongo/mongoConnection.js
+++ src/utils/mongo/mongoConnection.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 config()
 
-export const mongoConnection = () => mongoose.connect(DB_URL)
+export const mongoConnection = () => mongoose.connect("mongodb+srv://ldanko:Iz8ddD4Iu1kR7vEf@productmanager.0scft9y.mongodb.net/?retryWrites=true&w=majority")
